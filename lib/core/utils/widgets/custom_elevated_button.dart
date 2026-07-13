421f9fe27a1aed93a596ab3d5fb9a24bb574657e
Ahmed <ahmeddkhader111@gmail.com>
2026-07-13 16:48:47 +0300

    update elevated button

--- lib/core/utils/widgets/custom_elevated_button.dart
+++ lib/core/utils/widgets/custom_elevated_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:apk_scanner/core/utils/colors/app_colors.dart';
+
 class CustomElevatedButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
@@ -33,19 +35,26 @@ class CustomElevatedButton extends StatelessWidget {
       height: height,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor,
-          foregroundColor: foregroundColor,
+          backgroundColor: backgroundColor ?? context.primaryColor,
+          foregroundColor:
+              foregroundColor ??
+              (Theme.of(context).brightness == Brightness.dark
+                  ? AppColors.blackColor
+                  : AppColors.whiteColor),
           elevation: elevation ?? 0,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(radius ?? 24),
           ),
-          padding: padding ??
+          padding:
+              padding ??
               const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
         ),
         onPressed: onPressed,
         child: Text(
           text,
-          style: textStyle,
+          style:
+              textStyle ??
+              const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
       ),
     );
