e1192bca8b3f288be0c1423dc40ad211471de0cb
Ahmed <ahmeddkhader111@gmail.com>
2026-07-13 16:12:33 +0300

    update app colcor

--- lib/core/utils/colors/app_colors.dart
+++ lib/core/utils/colors/app_colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  // ===== Core neutrals =====
   static const Color whiteColor = Colors.white;
   static const Color blackColor = Colors.black;
   static const Color transparent = Colors.transparent;
