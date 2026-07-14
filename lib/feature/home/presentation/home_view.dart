bb57be30256ba4caa79e41c9cc36a48a85cf5e14
Ahmed <ahmeddkhader111@gmail.com>
2026-07-14 16:41:15 +0300

    update home view

--- lib/feature/home/presentation/home_view.dart
+++ lib/feature/home/presentation/home_view.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/material.dart';
+import '../../../core/utils/colors/app_colors.dart';
+import 'widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentIndex = 0;
+
+  // القائمة بدون const إجباري قبل القائمة أو الكلاس لمنع الأخطاء البرمجية
+  final List<Widget> _screens = [
+    const HomeViewBody(),
+    const Center(
+      child: Text('Scan Screen', style: TextStyle(color: Colors.white)),
+    ),
+    const Center(
+      child: Text('Reports Screen', style: TextStyle(color: Colors.white)),
+    ),
+    const Center(
+      child: Text('Profile Screen', style: TextStyle(color: Colors.white)),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'APK Scanner',
-          style: TextStyle(color: Color(0xFF00FF41)),
+      backgroundColor: const Color(0xFF0C0C0C),
+      body: SafeArea(child: _screens[_currentIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF0C0C0C),
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: AppColors.textHint,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 11,
+          fontWeight: FontWeight.bold,
         ),
-        backgroundColor: const Color(0xFF1A1A1A),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to APK Scanner',
-          style: TextStyle(color: Color(0xFF00FF41), fontSize: 20),
-        ),
+        unselectedLabelStyle: const TextStyle(fontSize: 11),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description_outlined),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
