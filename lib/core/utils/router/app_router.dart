4cceec76de82d3eb2101b8d30cb34960713b286d
tokaahmed354 <tokaahmed34567@gmail.com>
2026-07-27 17:20:46 +0200

    add redirect

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,13 +1,27 @@
 import 'package:apk_scanner/core/utils/router/routes_name.dart';
+import 'package:apk_scanner/core/utils/service_locator/service_locator.dart';
 import 'package:apk_scanner/core/utils/widgets/main_navigation_bottom.dart';
 import 'package:apk_scanner/feature/auth/presentation/login_view.dart';
 import 'package:apk_scanner/feature/auth/register_view.dart';
-import 'package:apk_scanner/feature/home/presentation/home_view.dart';
 import 'package:apk_scanner/feature/splash/presentation/splash_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static final router = GoRouter(
+    redirect: (context, state) {
+      final user = getIt.get<FirebaseAuth>().currentUser;
+
+      if (user != null && state.name != RoutesName.mainNavigation) {
+        return RoutesName.mainNavigation;
+      }
+
+      if (user == null && state.name == RoutesName.mainNavigation) {
+        return RoutesName.login;
+      }
+
+      return null;
+    },
     routes: [
       GoRoute(
         path: RoutesName.splash,
